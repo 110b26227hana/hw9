88ad60e998c16515d1b3470857ca924cbd966a04
110b26227hana <114041580+110b26227hana@users.noreply.github.com>
2022-10-28 20:44:18 +0800

    Update Class17.java

--- Class17.java
+++ Class17.java
@@ -1,1 +1,10 @@
+class Namecard
+     {
+          private String name;
+          private String address;
+          private phone data;
+     }
 
+         Namecard first=new Namecard ("Andy","123City","2345-6789","0911*336600");
+   
+         first.show();
